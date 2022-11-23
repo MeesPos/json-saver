17742d3ffaaf8dc25973b4fd043739c572106d66
MeesPos <meespostma_@hotmail.com>
2022-11-23 15:28:07 +0100

    Added importer

---
+++ app/Console/Commands/ImportJson.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Creditcard;
+use App\Models\Person;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Date;
+
+class ImportJson extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:json';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import JSON to the database.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if (file_exists(resource_path('challenge.json'))) {
+            $persons = json_decode(file_get_contents(resource_path('challenge.json')));
+
+            foreach ($persons as $person) {
+                $personModel = Person::query()
+                    ->updateOrCreate([
+                        'account' => $person->account
+                    ], [
+                        'name' => $person->name,
+                        'address' => $person->address,
+                        'checked' => $person->checked,
+                        'description' => $person->description,
+                        'interest' => $person->interest,
+                        'date_of_birth' => date('Y-m-d', strtotime($person->date_of_birth)),
+                        'email' => $person->email,
+                        'account' => $person->account
+                    ]);
+
+                Creditcard::query()
+                    ->updateOrCreate([
+                        'number' => $person->credit_card->number
+                    ], [
+                        'person_id' => $personModel->getKey(),
+                        'type' => $person->credit_card->type,
+                        'number' => $person->credit_card->number,
+                        'name' => $person->credit_card->name,
+                        'expiration_date' => $person->credit_card->expirationDate
+                    ]);
+            }
+
+            return Command::SUCCESS;
+        }
+
+        return Command::INVALID;
+    }
+}
