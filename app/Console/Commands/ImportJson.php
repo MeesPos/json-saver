f8ff3007175f2030eaffa4934f23d7b781b5d225
MeesPos <meespostma_@hotmail.com>
2022-11-23 15:42:11 +0100

    Added that records of persons under 18 or over 65 are skipped

--- app/Console/Commands/ImportJson.php
+++ app/Console/Commands/ImportJson.php
@@ -35,30 +35,35 @@ class ImportJson extends Command
             $persons = json_decode(file_get_contents(resource_path('challenge.json')));
 
             foreach ($persons as $person) {
-                $personModel = Person::query()
-                    ->updateOrCreate([
-                        'account' => $person->account
-                    ], [
-                        'name' => $person->name,
-                        'address' => $person->address,
-                        'checked' => $person->checked,
-                        'description' => $person->description,
-                        'interest' => $person->interest,
-                        'date_of_birth' => date('Y-m-d', strtotime($person->date_of_birth)),
-                        'email' => $person->email,
-                        'account' => $person->account
-                    ]);
+                $dateOfBirth = date('Y-m-d', strtotime($person->date_of_birth));
+                $age = Carbon::parse($dateOfBirth)->age;
 
-                Creditcard::query()
-                    ->updateOrCreate([
-                        'number' => $person->credit_card->number
-                    ], [
-                        'person_id' => $personModel->getKey(),
-                        'type' => $person->credit_card->type,
-                        'number' => $person->credit_card->number,
-                        'name' => $person->credit_card->name,
-                        'expiration_date' => $person->credit_card->expirationDate
-                    ]);
+                if (($age >= 18 && $age <= 65) || is_null($age)) {
+                    $personModel = Person::query()
+                        ->updateOrCreate([
+                            'account' => $person->account
+                        ], [
+                            'name' => $person->name,
+                            'address' => $person->address,
+                            'checked' => $person->checked,
+                            'description' => $person->description,
+                            'interest' => $person->interest,
+                            'date_of_birth' => $dateOfBirth,
+                            'email' => $person->email,
+                            'account' => $person->account
+                        ]);
+
+                    Creditcard::query()
+                        ->updateOrCreate([
+                            'number' => $person->credit_card->number
+                        ], [
+                            'person_id' => $personModel->getKey(),
+                            'type' => $person->credit_card->type,
+                            'number' => $person->credit_card->number,
+                            'name' => $person->credit_card->name,
+                            'expiration_date' => $person->credit_card->expirationDate
+                        ]);
+                }
             }
 
             return Command::SUCCESS;
