124498833bb66a459461327cfd559eec7182174d
MeesPos <meespostma_@hotmail.com>
2022-11-23 17:26:31 +0100

    Moved import job to a job

--- app/Console/Commands/ImportJson.php
+++ app/Console/Commands/ImportJson.php
@@ -2,11 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Creditcard;
-use App\Models\Person;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
+use App\Jobs\ImportJson as ImportJsonJob;
 
 class ImportJson extends Command
 {
@@ -31,50 +28,6 @@ class ImportJson extends Command
      */
     public function handle()
     {
-        $file = 'challenge.json';
-
-        if (file_exists(resource_path($file))) {
-            $persons = json_decode(file_get_contents(resource_path($file)));
-
-            foreach ($persons as $index => $person) {
-                if (Cache::get($file . '-index') < $index) {
-                    $dateOfBirth = date('Y-m-d', strtotime($person->date_of_birth));
-                    $age = Carbon::parse($dateOfBirth)->age;
-
-                    if (($age >= 18 && $age <= 65) || is_null($age) ) {
-                        $personModel = Person::query()
-                            ->create([
-                                'name' => $person->name,
-                                'address' => $person->address,
-                                'checked' => $person->checked,
-                                'description' => $person->description,
-                                'interest' => $person->interest,
-                                'date_of_birth' => $dateOfBirth,
-                                'email' => $person->email,
-                                'account' => $person->account
-                            ]);
-
-                        Creditcard::query()
-                            ->create([
-                                'person_id' => $personModel->getKey(),
-                                'type' => $person->credit_card->type,
-                                'number' => $person->credit_card->number,
-                                'name' => $person->credit_card->name,
-                                'expiration_date' => $person->credit_card->expirationDate
-                            ]);
-
-                        Cache::put($file . '-index', $index);
-                    }
-
-                    $this->info($person->name . ' imported');
-                }
-            }
-
-            Cache::delete($file . '-index');
-
-            return Command::SUCCESS;
-        }
-
-        return Command::INVALID;
+        ImportJsonJob::dispatch();
     }
 }
---
+++ app/Jobs/ImportJson.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Creditcard;
+use App\Models\Person;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class ImportJson implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $file = 'challenge.json';
+
+        if (file_exists(resource_path($file))) {
+            $persons = json_decode(file_get_contents(resource_path($file)));
+
+            foreach ($persons as $index => $person) {
+                if (Cache::get($file . '-index') < $index) {
+                    $dateOfBirth = date('Y-m-d', strtotime($person->date_of_birth));
+                    $age = Carbon::parse($dateOfBirth)->age;
+
+                    if (($age >= 18 && $age <= 65) || is_null($age) ) {
+                        $personModel = Person::query()
+                            ->create([
+                                'name' => $person->name,
+                                'address' => $person->address,
+                                'checked' => $person->checked,
+                                'description' => $person->description,
+                                'interest' => $person->interest,
+                                'date_of_birth' => $dateOfBirth,
+                                'email' => $person->email,
+                                'account' => $person->account
+                            ]);
+
+                        Creditcard::query()
+                            ->create([
+                                'person_id' => $personModel->getKey(),
+                                'type' => $person->credit_card->type,
+                                'number' => $person->credit_card->number,
+                                'name' => $person->credit_card->name,
+                                'expiration_date' => $person->credit_card->expirationDate
+                            ]);
+
+                        Cache::put($file . '-index', $index);
+                    }
+                }
+            }
+
+            Cache::delete($file . '-index');
+        }
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
